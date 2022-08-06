9f96624e0b066e8ea4888760ccd4324aa70d6b7d
HeheBoi1234 <108604181+HeheBoi1234@users.noreply.github.com>
2022-08-06 17:01:52 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+-- local list = game:HttpGet('https://dakgfksjdhfjhjghshd1234132412.000webhostapp.com/?authtoken=DitMeThangLozAdminVietNamPiece&type=get&Name='..game:GetService("Players").LocalPlayer.Name)
+-- if list == "true" then
+--     game:GetService("Players").LocalPlayer:Kick("You in blacklist VietHub")
+-- else
+if game.PlaceId == 2116452049 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/viet452009/hellminer-linux/main/2116452049"))()
+elseif game.PlaceId == 4587545091 or game.PlaceId == 7258239416 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HeheBoi1234/abcxyzhehe/main/4587545091noHEHE"))()
+elseif game.PlaceId == 6329844902 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HeheBoi1234/abcxyzhehe/main/6329844902ohNo"))()
+elseif game.PlaceId == 8123652942 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HeheBoi1234/abcxyzhehe/main/8123652942wtfman"))()
+end
+-- end
+    
